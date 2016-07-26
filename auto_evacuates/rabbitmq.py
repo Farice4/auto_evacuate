4c7c81bdab4f8ca84f8e294805ddd09c19b2a80f
zhouyang1222 <419606291@qq.com>
2016-07-26 19:16:11 +0800

    add try every rabbit host connect zhouyang <yang.zhou1222@gmail.com>

--- auto_evacuates/rabbitmq.py
+++ auto_evacuates/rabbitmq.py
@@ -1,32 +1,46 @@
 import pika
+import ConfigParser
 
 
 class Py_Rabbitmq(object):
     def __init__(self):
         self.user = 'nova'
         self.pwd = 'irxscf28'
-        self.localhost = '127.0.0.1'
         self.port = 5673
-        self.credential = pika.PlainCredentials(self.user, self.pwd)
-        self.pid = pika.ConnectionParameters(
-                self.localhost, self.port, '/', self.credential)
-        self.connection = pika.BlockingConnection(self.pid)
-        self.channel = self.connection.channel()
         self.msg_list = []
 
+    def rbt_connection(self):
+        cf = ConfigParser.ConfigParser()
+        cf.read("/etc/nova/nova.conf")
+        hosts = cf.get("DEFAULT", "rabbit_hosts")
+        hosts_list = hosts.split(',')
+        for host_list in hosts_list:
+            host = host_list.split(':')
+            credential = pika.PlainCredentials(self.user, self.pwd)
+            try:
+                pid = pika.ConnectionParameters(
+                        host[0], self.port, '/', credential)
+                connection = pika.BlockingConnection(pid)
+                channel = connection.channel()
+                return channel
+            except Exception:
+                print 'error'
+
     def callback(self, ch, method, properties, body):
-                self.msg_list.append(body)
+        self.msg_list.append(body)
 
     def publish(self, msg_list):
-        self.channel.exchange_declare(exchange='first', type='fanout')
-        self.channel.queue_declare(queue='fence_nodes')
-        self.channel.queue_bind(exchange='first', queue='fence_nodes')
+        channel = self.rbt_connection()
+        channel.exchange_declare(exchange='first', type='fanout')
+        channel.queue_declare(queue='fence_nodes')
+        channel.queue_bind(exchange='first', queue='fence_nodes')
         for msg in msg_list:
-            self.channel.basic_publish(
+            channel.basic_publish(
                     exchange='first', routing_key='', body=msg)
 
     def consume(self):
-        self.channel.queue_declare(queue='fence_nodes')
-        self.channel.basic_consume(
+        channel = self.rbt_connection1()
+        channel.queue_declare(queue='fence_nodes')
+        channel.basic_consume(
                 self.callback, queue='fence_nodes', no_ack=True)
         return self.msg_list
