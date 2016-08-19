ee8d7a97728ee031ecde669bfec493bba844ff76
Dunrong Huang <dunrong.huang@eayun.com>
2016-08-19 16:37:19 +0800

    fix: 正在fence中的节点不需要检测
    增加一个记录正在fence中的节点列表：
    1. 如果发现节点出错，做fence之前需要先把节点加入到该列表中， 防止下次检测的时候再检测该节点然后做evacuate等操作出错
    2. 每次重新发起检测的时候，如果节点恢复正常了， 需要把就节点从列表中删除
    3. 如果在做fence和evacuate等操作时出错，则必须将节点从列表中删除
    
    Signed-off-by: Dunrong Huang <dunrong.huang@eayun.com>

--- auto_evacuates/manage.py
+++ auto_evacuates/manage.py
@@ -16,14 +16,61 @@ NOVACLIENT_AUTH = {'username': CONF.get('novaclient', 'username'),
                    'project_id': CONF.get('novaclient', 'project_id'),
                    'auth_url': CONF.get('novaclient', 'auth_url'),
                    'version': CONF.get('novaclient', 'version')}
+# fake enum implementaion
+FENCE_TYPE_POWER, FENCE_TYPE_NETWORK, FENCE_TYPE_SERVICE = range(3)
+
+
+class FencedNode(object):
+    def __init__(self):
+        # TODO:get from rabbitmq
+        self.power = set()
+        self.network = set()
+        self.service = set()
+
+    def _find(self, type):
+        if type == FENCE_TYPE_POWER:
+            return self.power
+        elif type == FENCE_TYPE_NETWORK:
+            return self.network
+        elif type == FENCE_TYPE_SERVICE:
+            return self.service
+
+    def remove(self, nodes, type):
+        obj = self._find(type)
+        for n in nodes:
+            try:
+                obj.remove(n)
+            except KeyError:
+                logger.warn('type %s no such node: %s', type, n)
+
+    def add(self, nodes, type):
+        obj = self._find(type)
+        for n in nodes:
+            obj.add(n)
+
+    def remove_one(self, node, type):
+        obj = self._find(type)
+        try:
+            obj.remove(node)
+        except KeyError:
+            logger.warn('type %s no such node: %s', type, node)
+
+    def add_one(self, node, type):
+        obj = self._find(type)
+        obj.add(node)
+
+    def get(self, type):
+        obj = self._find(type)
+        return list(obj)
 
 
 class Manager(object):
     def __init__(self):
         self.ipmi_objs = []
-        self.pool = eventlet.GreenPool()  # give a greenthread size?
+        self.fenced_node = FencedNode()
         self.nova_compute_client = NovaComputeClient(NOVACLIENT_AUTH)
         self.fence_op = FenceOperation(self.nova_compute_client)
+        self.pool = eventlet.GreenPool()  # give a greenthread size?
 
     def run(self):
         """check cluster status every 30s, if some serious error
@@ -56,6 +103,28 @@ class Manager(object):
                 # TODO, catch speical exception, raise unknow excep to caller?
                 logger.error("Failed to auto evacuate: %s" % e)
             eventlet.sleep(30)
+
+    def _update_fence_and_error_node(self, type, error_nodes):
+        """
+        This function do following things:
+        - remove nodes from FENCE_NODE that are not in the error_nodes
+        - update error_nodes: if nodes are in FENCE_NODE, no need to
+           handle them
+        @return error_nodes that contains error nodes which are not in
+                FENCE_NODE
+        """
+        # update FENCE_NODE
+        error_node_names = [n['hostname'] for n in error_nodes]
+        correct_nodes = [n for n in self.fenced_node.get(type)
+                         if n not in error_node_names]
+        if correct_nodes:
+            logger.warn('%s has been up, remove from fenced_node',
+                        correct_nodes)
+            self.fenced_node.remove(correct_nodes, type)
+
+        # update error_nodes
+        return [n for n in error_nodes
+                if n['hostname'] not in self.fenced_node.get(type)]
 
     def _init_ipmi(self, conf):
         """ ipmi_objs = [
@@ -103,23 +172,33 @@ class Manager(object):
             logger.error("IPMI Error is: %s" % e)
         except Exception as e:
             logger.error("Unknown Error %s" % e)
+        finally:
+            return self._update_fence_and_error_node(FENCE_TYPE_POWER,
+                                                     error_nodes)
 
     def _handle_ipmi_error(self, error_nodes):
         self.pool.imap(self._handle_ipmi_one, error_nodes)
 
     def _handle_ipmi_one(self, node):
+        # TODO: ensure power is off again
+        self.fenced_node.add_one(node['hostname'], FENCE_TYPE_POWER)
         try:
             self.fence_op.fence_node_for_ipmi(node['hostname'])
             logger.info("%s fence successed" % node['hostname'])
         except NovaClientError as e:
             logger.error("NovaClientError lead to fence %s failed:%s"
                          % (node['hostname'], e))
+            self.fenced_node.remove_one(node['hostname'], FENCE_TYPE_POWER)
         except Exception as e:
             logger.error("UnknownError lead to fence %s failed:%s"
                          % (node['hostname'], e))
+            self.fenced_node.remove_one(node['hostname'], FENCE_TYPE_POWER)
 
-        # check success or not
-        self.nova_compute_client.evacuate_all(node['hostname'])
+        if self.nova_compute_client.evacuate_all(node['hostname']):
+            logger.info('evacuate instances from %s successed'
+                        % node['hostname'])
+        else:
+            self.fenced_node.remove_one(node['hostname'], FENCE_TYPE_POWER)
 
     def _check_network(self):
         return []
